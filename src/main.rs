a84c3d92f90ffee5ff40c70e92f02625e8ce3c51
Kirill Pimenov <kirushik@gmail.com>
2016-03-23 17:15:42 +0100

    Very stupid example bot

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,26 @@
+//extern crate rustc_serialize;
+extern crate telegram_bot;
+
+use telegram_bot::*;
+//use rustc_serialize::json;
+
 fn main() {
-    println!("Hello, world!");
+    let api = Api::from_env("TELEGRAM_BOT_TOKEN").unwrap();
+    println!("getMe: {:?}", api.get_me());
+
+    let mut listener = api.listener(ListeningMethod::LongPoll(None));
+
+    listener.listen(|u| {
+        if let Some(m) = u.message {
+            if let MessageType::Text(_) = m.msg {
+                try!(api.send_message(
+                        m.chat.id(),
+                        format!("Hi, {}!", m.from.first_name),
+                        None, None, None, None)
+                    );
+            }
+        }
+
+        Ok(ListeningAction::Continue)
+    });
 }
